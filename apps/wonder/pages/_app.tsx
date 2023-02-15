fb433839732488cdbf1b73c11c45cf02124d9234
andresrivera <andresrivera@habi.co>
2023-02-14 21:40:04 -0500

    feat: fix images pwa

--- apps/wonder/pages/_app.tsx
+++ apps/wonder/pages/_app.tsx
@@ -20,30 +20,31 @@ function CustomApp({ Component, pageProps }: AppProps) {
         <meta name="msapplication-TileColor" content="#2B5797" />
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#000000" />
+        <link rel="icon" type="image/x-icon" href="/assets/icons/logo-ico-04.jpg"/>
 
-        <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/icons/touch-icon-ipad.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/icons/touch-icon-iphone-retina.png" />
-        <link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" />
+        <link rel="apple-touch-icon" href="/assets/icons/logo-ico-04.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/assets/icons/logo-ico-02.jpg" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/icons/logo-ico-01.jpg" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/assets/icons/logo-ico-03.jpg" />
 
-        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/icons/logo-ico-04.jpg" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/icons/logo-ico-05.jpg" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
         <link rel="shortcut icon" href="/favicon.ico" />
 
         <meta name="twitter:card" content="summary" />
-        <meta name="twitter:url" content="https://yourdomain.com" />
+        <meta name="twitter:url" content="https://wonder-peach.vercel.app/" />
         <meta name="twitter:title" content="PWA App" />
         <meta name="twitter:description" content="Best PWA App in the world" />
-        <meta name="twitter:image" content="https://yourdomain.com/icons/android-chrome-192x192.png" />
-        <meta name="twitter:creator" content="@DavidWShadow" />
+        <meta name="twitter:image" content="https://wonder-peach.vercel.app/assets/icons/logo-ico-01.jpg" />
+        <meta name="twitter:creator" content="@bysebastean" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="PWA App" />
         <meta property="og:description" content="Best PWA App in the world" />
         <meta property="og:site_name" content="PWA App" />
-        <meta property="og:url" content="https://yourdomain.com" />
-        <meta property="og:image" content="https://yourdomain.com/icons/apple-touch-icon.png" />
+        <meta property="og:url" content="https://wonder-peach.vercel.app/" />
+        <meta property="og:image" content="https://wonder-peach.vercel.app/assets/icons/logo-ico-01.jpg" />
 
         {/* <link rel='apple-touch-startup-image' href='/images/apple_splash_2048.png' sizes='2048x2732' />
         <link rel='apple-touch-startup-image' href='/images/apple_splash_1668.png' sizes='1668x2224' />
